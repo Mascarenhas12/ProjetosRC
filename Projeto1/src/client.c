bbc85522e51b7322efe6458cf0647c3c12c8ebd8
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2019-10-27 23:17:17 +0000

    Finished Server and single client socket connection; Fixed makefile

--- Projeto1/src/client.c
+++ Projeto1/src/client.c
@@ -9,15 +9,64 @@
  * ================================================
  */
 
-#include <sys/select.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "../hdr/server.h"
+
 #define MAXCHAR 4096
 
-int main(int argc, char const *argv[]) {
-  /* code */
+int main(int argc, char const *argv[])
+{
+  int fd_clientSock;
+
+  int clientPort;
+  char clientAddr[256] = "127.0.0.1";
+
+  struct sockaddr_in clientSock_addr;
+
+  char buffer[MAXCHAR];
+
+  if (argc < 3 || strcmp(argv[1], "localhost") || atoi(argv[2]) > 65535)
+  {
+    perror("Wrong arguments! Expected a server port!");
+    return -1;
+  }
+
+  /*clientAddr = "127.0.0.1";*/
+  clientPort = atoi(argv[2]);
+
+  if ((fd_clientSock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("Error while creating client socket!");
+    return -1;
+  }
+
+  bzero((char *) &clientSock_addr, sizeof(clientSock_addr));
+  clientSock_addr.sin_family = AF_INET;
+  clientSock_addr.sin_port = htons(clientPort);
+  clientSock_addr.sin_addr.s_addr = inet_addr(clientAddr);
+
+  if ((connect(fd_clientSock, (struct sockaddr*) &clientSock_addr, sizeof(clientSock_addr))) == -1)
+  {
+    perror("Error doing connect in client!");
+    return -1;
+  }
+
+  /*if ((recv(fd_clientSock, &buffer, sizeof(buffer), 0)) == -1)
+  {
+    perror("Error receiving from server!");
+    return -1;
+  }
+
+  printf("%s\n", buffer);*/
+
+  close(fd_clientSock);
   return 0;
 }
--- Projeto1/src/server.c
+++ Projeto1/src/server.c
@@ -9,61 +9,94 @@
  * ================================================
  */
 
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include "../hdr/server.h"
+
 #define MAXCHAR 4096
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
+  int serverPort;
 
-  fd_set readFds;
-  fd_set writeFds;
+  /* fd_set readFds; */
+  /* fd_set writeFds; */
 
-  int sockFd;
-  int clientFd;
+  int fd_serverSock;
+  int fd_clientSock;
 
-  struct sockaddr sockAddr;
+  struct sockaddr_in serverSock_addr;
+  struct sockaddr_in clientSock_addr;
+  socklen_t clientSock_addr_len;
 
-  if ((sockFd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) { /* int socket(int domain, int type, int protocol); */
-    perror("Error while doing select!");
+  char message[MAXCHAR] = "Connected to server!";
+
+  /*1 - 1023 so pode correr em root duvida*/
+  if (argc < 2 || atoi(argv[1]) > 65535)
+  {
+    perror("Wrong arguments! Expected a server port!");
     return -1;
   }
 
-  if ((bind(sockFd, &sockAddr, )) == -1) { /* int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen); */
+  serverPort = atoi(argv[1]);
+
+  /* int socket(int domain, int type, int protocol); */
+  if ((fd_serverSock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("Error while creating server socket!");
+    return -1;
+  }
+
+  bzero((char *) &serverSock_addr, sizeof(serverSock_addr));
+  serverSock_addr.sin_family = AF_INET;
+  serverSock_addr.sin_port = htons(serverPort);
+  serverSock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /* int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen); */
+  if ((bind(fd_serverSock, (struct sockaddr*) &serverSock_addr, sizeof(serverSock_addr))) == -1)
+  {
     perror("Error while doing bind!");
     return -1;
   }
 
-  if ((listen(sockFd, )) == -1) {
+  /* int listen(int sockfd, int backlog); */
+  if ((listen(fd_serverSock, 1)) == -1)
+  {
     perror("Error while doing listen!");
     return -1;
   }
-
+  /*
   FD_ZERO(&readFds);
   FD_ZERO(&writeFds);
 
   FD_SET(fileDescriptor, &readFds);
   FD_SET(fileDescriptor, &write);
-
+  */
   /* select code */
 
   /*FD_ISSET()*/
 
-  if ((clientFd = accept(sockFd, , )) == -1) {
+  clientSock_addr_len = sizeof(clientSock_addr);
+  if ((fd_clientSock = accept(fd_serverSock, (struct sockaddr*) &clientSock_addr, &clientSock_addr_len)) == -1)
+  {
     perror("Error while doing accept!");
     return -1;
   }
 
+  /* ssize_t send(int sockfd, const void *buf, size_t len, int flags); */
+  if ((send(fd_clientSock, message, sizeof(message), 0)) == -1)
+  {
+    perror("Error while sending message in server!");
+    return -1;
+  }
 
-
-
-
-
-
+  close(fd_serverSock);
 
   return 0;
 }
