bbc56f3c7a18ad0f9ace92d23b042dd1bc29efbd
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2019-11-21 22:25:33 +0000

    Started adding pipeline logic, send packets and recv packets;

--- Projeto2/file-receiver.c
+++ Projeto2/file-receiver.c
@@ -16,9 +16,9 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "packet-format.h"
 
-#define MAX_WINDOW_SIZE 32
-#define CHUNK_SIZE 1000
+#define MAX_CHUNK_SIZE 1000
 
 int int main(int argc, char const *argv[])
 {
@@ -29,8 +29,12 @@ int int main(int argc, char const *argv[])
 	struct sockaddr_storage senderSock_addr;
 	socklen_t senderSock_addr_len;
 
-	int n;
-	char chunk[CHUNK_SIZE];
+	int window_size;
+	char pipeline[MAX_WINDOW_SIZE];
+	data_pkt_t* chunk;
+
+	int seq_num;
+	int ack_mask;
 
 	/* ======================================================================================== */
 	/* Argument verification                                                                    */
@@ -42,13 +46,13 @@ int int main(int argc, char const *argv[])
 		exit(-1);
 	}
 
-	if ((port = atoi(argv[3])) > 65535)
+	if ((port = atoi(argv[2])) > 65535)
 	{
 		perror("file-receiver:Invalid port number!");
 		exit(-1);
 	}
 
-	if (atoi(argv[4]) > MAX_WINDOW_SIZE)
+	if ((window_size = atoi(argv[4])) > MAX_WINDOW_SIZE)
 	{
 		perror("file-receiver:Invalid window size!");
 		exit(-1);
@@ -68,9 +72,9 @@ int int main(int argc, char const *argv[])
 	senderSock_addr_len = sizeof(struct sockaddr_storage);
 
 	memset(&receiverSock_addr, 0, sizeof(receiverSock_addr));
-	serverSock_addr.sin_family = AF_INET;
-	serverSock_addr.sin_port = htons(port);
-	serverSock_addr.sin_addr.s_addr = INADDR_ANY;
+	receiverSock_addr.sin_family = AF_INET;
+	receiverSock_addr.sin_port = htons(port);
+	receiverSock_addr.sin_addr.s_addr = INADDR_ANY;
 
 	if ((bind(receiverSock, (struct sockaddr*) &receiverSock_addr, sizeof(receiverSock_addr))) == -1)
 	{
@@ -79,21 +83,64 @@ int int main(int argc, char const *argv[])
 		exit(-1);
 	}
 
-	//ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
-	if ((n = recvfrom(receiverSock, (char*) chunk, CHUNK_SIZE, 0, (struct sockaddr*) &senderSock_addr, &senderSock_addr_len)) == -1)
-	{
-		perror("file-receiver:Error while receiving!");
-		close(receiverSock);
-		exit(-1);
-	}
+	seq_num = 0;
+	ack_mask = 0;
 
-	// ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
-	if (sendto(receiverSock, (char*) b, CHUNK_SIZE, 0, (struct sockaddr*) &senderSock_addr, &senderSock_addr_len) == -1)
+	do
 	{
-		perror("file-receiver:Error while sending ACK!");
-		close(receiverSock);
-		exit(-1);
-	}
-	
+		//ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+		if (recvfrom(receiverSock, (data_pkt_t*) chunk, sizeof(data_pkt_t), 0, (struct sockaddr*) &senderSock_addr, &senderSock_addr_len) == -1)
+		{
+			perror("file-receiver:Error while receiving!");
+			close(receiverSock);
+			exit(-1);
+		}
+
+		ack_pkt_t ack;
+		ack.seq_num = ++seq_num;
+		ack.selective_acks = 0;
+
+		// ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+		if (sendto(receiverSock, &ack, sizeof(ack_pkt_t), 0, (struct sockaddr*) &senderSock_addr, &senderSock_addr_len) == -1)
+		{
+			perror("file-receiver:Error while sending ACK!");
+			close(receiverSock);
+			exit(-1);
+		}
+	} while (sizeof(chunk->data) == MAX_CHUNK_SIZE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	exit(0);
 }
